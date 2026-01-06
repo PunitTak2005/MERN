20fde98d3fcb09a31037b6ffa84346d7e9da21d3
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2026-01-06 09:37:11 +0530

    Update two-pointer algorithm.js

--- two-pointer algorithm.js
+++ two-pointer algorithm.js
@@ -3,18 +3,16 @@ function findAllPairs(arr, k) {
     let i = 0;
     let j = arr.length - 1;
 
-    while (i < j) {  // ensures i !== j
+    while (i < j) { 
         const sum = arr[i] + arr[j];
 
         if (sum === k) {
-            pairs.push([i, j]);  // or push([arr[i], arr[j]])
+            pairs.push([i, j]);  
             
             i++;
             j--;
 
-            // Optional: skip duplicates for unique value pairs
-            // while (i < j && arr[i] === arr[i-1]) i++;
-            // while (i < j && arr[j] === arr[j+1]) j--;
+           
         } else if (sum < k) {
             i++;
         } else {
@@ -25,7 +23,7 @@ function findAllPairs(arr, k) {
     return pairs;
 }
 
-// Example usage
+
 const arr = [1, 3, 4, 5, 6, 8, 9];
 const k = 9;
-console.log(findAllPairs(arr, k));  // [[1, 6], [3, 4]]  (indices where 3+6=9, 4+5=9)
+console.log(findAllPairs(arr, k)); 
